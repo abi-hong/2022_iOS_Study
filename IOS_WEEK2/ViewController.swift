bbc48c74604947fb6c44014615128034d1c58777
abi-hong <68041042+abi-hong@users.noreply.github.com>
2022-01-07 16:13:28 +0900

    WEEK2(Tab bar, Layout, StackView, ScrollView)

---
+++ IOS_WEEK2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_WEEK2
+//
+//  Created by 홍희수 on 2022/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
